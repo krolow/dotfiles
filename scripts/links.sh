29e2e3bfb982447908c4f204934992d357a2d12e
Vinícius Krolow <krolow@gmail.com>
2016-03-11 01:15:47 -0300

    Add basic config for symlink and tmux

---
+++ scripts/links.sh
@@ -0,0 +1,17 @@
+cd "$(dirname "$0")"
+DOTFILES=$(dirname "$(pwd)")
+
+links=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
+
+for link in $links; do
+  target="${HOME}/.$(basename $link ".symlink")"
+  echo "target: ${target}"
+  echo "link: ${link}"
+
+  if [ -e $target ]; then
+	echo "Skip ${target} ..."
+  else
+	echo "Creating symlink for ${target}"
+	ln -s $link $target
+  fi
+done
