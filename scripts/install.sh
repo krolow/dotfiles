993215b332b29d43e8d78a78c4c46a9ff28c286f
Vinícius Krolow <vinicius.krolow@bravi.com.br>
2016-03-14 14:27:30 -0300

    Add tmux plugins

--- scripts/install.sh
+++ scripts/install.sh
@@ -36,13 +36,13 @@ gconftool-2 --set /apps/gnome-terminal/profiles/one-dark/font --type string "Kna
 
 # install vim configuration
 source "./vim.sh"
-
+# install tmux plugin
+source "./tmux.sh"
 
 # install oh-my-zsh
 if [ ! -d "${HOME}/.oh-my-zsh" ]; then
   git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
-
 
 # links
 source "${DIR}/links.sh"
