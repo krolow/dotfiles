648b4e5ee97be19d71d4c159e67a3922c379abf7
Vinícius Krolow <krolow@gmail.com>
2016-07-25 16:02:26 -0300

    Add alias to ubuntu version :facepunch:

--- alias.sh
+++ alias.sh
@@ -3,3 +3,4 @@ alias docker-ps='docker ps -a'
 alias docker-clean='docker rm $(docker ps -aq)'
 alias docker-force-clean='docker rm -f $(docker ps -aq)'
 alias docker-compose-run="docker-compose run --service-ports"
+alias ubuntu-version="cat /etc/issue"
