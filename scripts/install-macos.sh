c47341f990442d9397bdf21c4fbfbd7136657dbe
Vinícius Krolow <krolow@gmail.com>
2016-08-04 11:39:34 -0300

    Add iterm theme

--- scripts/install-macos.sh
+++ scripts/install-macos.sh
@@ -24,11 +24,11 @@ defaults write -g InitialKeyRepeat -int 10
 # install iterm profile
 if [ ! -d "${DIR}/../macos/themes/iterm" ]; then
   mkdir -p $DIR/../macos/themes/iterm/
-  git clone https://github.com/nathanbuchar/atom-one-dark-terminal.git $DIR/../macos/themes/iterm/one-dark
+  git clone https://github.com/joshdick/onedark.vim $DIR/../macos/themes/iterm/one-dark
 fi
 
 defaults write com.apple.terminal FocusFollowsMouse -bool true
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 defaults write com.googlecode.iterm2 HideTab -bool true
 
-open "${DIR}/../macos/themes/iterm/one-dark/scheme/iterm/One Dark.itermcolors"
+open "${DIR}/../macos/themes/iterm/one-dark/One Dark.itermcolors"
