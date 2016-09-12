feac72728bccd976dc404a6c4034809d94a9f7d8
Vinícius Krolow <krolow@gmail.com>
2016-09-12 09:59:00 -0300

    Add docker-compose alias to run command no-deps

--- alias.sh
+++ alias.sh
@@ -3,4 +3,5 @@ alias docker-ps='docker ps -a'
 alias docker-clean='docker rm $(docker ps -aq)'
 alias docker-force-clean='docker rm -f $(docker ps -aq)'
 alias docker-compose-run="docker-compose run --service-ports --rm"
+alias docker-compose-command="docker-compose run --rm --no-deps"
 alias ubuntu-version="cat /etc/issue"
