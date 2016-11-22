815d40c9b389d06c50e063f5961783098c6662a5
Vinícius Krolow <krolow@gmail.com>
2016-11-22 11:39:55 -0200

    Add alias to dnsmask docker

--- alias.sh
+++ alias.sh
@@ -5,3 +5,4 @@ alias docker-force-clean='docker rm -f $(docker ps -aq)'
 alias docker-compose-run="docker-compose run --service-ports --rm"
 alias docker-compose-command="docker-compose run --rm --no-deps"
 alias ubuntu-version="cat /etc/issue"
+alias docker-dev-dns="docker run -d --name dnsmasq -p 53535:53/tcp -p 53535:53/udp --cap-add=NET_ADMIN andyshinn/dnsmasq --address=/dev/127.0.0.1"
