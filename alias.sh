30b69370942758d2ab4c3e9b493731bc1518f575
Vinícius Krolow <krolow@gmail.com>
2017-07-14 17:02:27 -0300

    Remove all

--- alias.sh
+++ alias.sh
@@ -2,7 +2,7 @@
 alias docker-ps='docker ps -a'
 alias docker-clean='docker rm $(docker ps -aq)'
 alias docker-force-clean='docker rm -f $(docker ps -aq)'
-alias docker-compose-run="docker-compose run --service-ports --rm"
+alias docker-compose-run="docker-compose run --service-ports --rm --all"
 alias docker-compose-command="docker-compose run --rm --no-deps"
 alias ubuntu-version="cat /etc/issue"
 alias docker-dev-dns="docker run -d --name dnsmasq -p 53535:53/tcp -p 53535:53/udp --cap-add=NET_ADMIN andyshinn/dnsmasq --address=/dev/127.0.0.1"
